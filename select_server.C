6c6729c80bb45fec67849a268dfdf1717b096b3f
Himanshu Singh <121677307+Himanshu-Singh-cpp@users.noreply.github.com>
2024-09-30 20:34:05 +0530

    Update select_server.C

--- select_server.C
+++ select_server.C
@@ -5,15 +5,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <sys/select.h>
-#include <stdbool.h>
 #include <dirent.h>
 #include <ctype.h>
+#include <errno.h>
 
 #define PORT 8080
 #define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
 
 typedef struct {
     char name[256];
@@ -26,16 +27,16 @@ typedef struct {
 void get_cpu_usage(ProcessInfo *proc) {
     char path[256], buffer[1024];
     sprintf(path, "/proc/%d/stat", proc->pid);
-    FILE *fp = fopen(path, "r");
-    if (fp == NULL) {
-        perror("fopen");
+    int fd = open(path, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
         return;
     }
-    
-    fread(buffer, 1, sizeof(buffer), fp);
+   
+    read(fd, buffer, sizeof(buffer));
     sscanf(buffer, "%*d %s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %ld %ld",
            proc->name, &proc->user_time, &proc->kernel_time);
-    fclose(fp);
+    close(fd);
 }
 
 // Function to find top two CPU-consuming processes
@@ -43,7 +44,7 @@ void find_top_processes(ProcessInfo procs[], int num_procs) {
     DIR *dir = opendir("/proc");
     struct dirent *entry;
     int count = 0;
-    
+
     while ((entry = readdir(dir)) != NULL && count < num_procs) {
         if (isdigit(*entry->d_name)) {
             procs[count].pid = atoi(entry->d_name);
@@ -54,114 +55,114 @@ void find_top_processes(ProcessInfo procs[], int num_procs) {
     closedir(dir);
 }
 
+// Function to compare processes by their total CPU time (user_time + kernel_time)
+int compare_processes(const void *a, const void *b) {
+    ProcessInfo *procA = (ProcessInfo *)a;
+    ProcessInfo *procB = (ProcessInfo *)b;
+    long total_time_a = procA->user_time + procA->kernel_time;
+    long total_time_b = procB->user_time + procB->kernel_time;
+    return total_time_b - total_time_a;
+}
+
 int main() {
-    int server_socket, client_socket, max_sd, activity, new_socket;
-    int client_sockets[MAX_CLIENTS] = {0};
-    struct sockaddr_in server_address;
+    int server_fd, new_socket, client_sockets[MAX_CLIENTS], max_sd, sd;
+    struct sockaddr_in address;
     fd_set readfds;
-    
-    int addrlen = sizeof(server_address);
+    char buffer[BUFFER_SIZE];
 
-    // Create a socket
-    if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+    // Initialize client sockets
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        client_sockets[i] = 0;
+    }
+
+    // Create server socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Set server address options
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-
-    // Bind the socket to the address
-    if(bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        perror("bind failed");
+    // Set socket options
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("Setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
-    // Start listening for incoming connections
-    if(listen(server_socket, 3) < 0) {
-        perror("listen");
+    // Bind the socket to the address and port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %d...\n", PORT);
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
 
-    while(true) {
-        // Clear the socket set and add server socket to set
+    printf("Listening on port %d\n", PORT);
+
+    int addrlen = sizeof(address);
+
+    while (1) {
+        // Clear the socket set
         FD_ZERO(&readfds);
-        FD_SET(server_socket, &readfds);
-        max_sd = server_socket;
 
-        // Add child sockets to set
+        // Add server socket to set
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
+
+        // Add client sockets to set
         for (int i = 0; i < MAX_CLIENTS; i++) {
-            int sd = client_sockets[i];
-            if (sd > 0) FD_SET(sd, &readfds);
-            if (sd > max_sd) max_sd = sd;
+            sd = client_sockets[i];
+            if (sd > 0) {
+                FD_SET(sd, &readfds);
+            }
+            if (sd > max_sd) {
+                max_sd = sd;
+            }
         }
 
         // Wait for activity on any of the sockets
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-
+        int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
         if ((activity < 0) && (errno != EINTR)) {
-            perror("select error");
+            perror("Select error");
         }
 
-        // If something happened on the master socket, it's an incoming connection
-        if (FD_ISSET(server_socket, &readfds)) {
-            if ((new_socket = accept(server_socket, (struct sockaddr *)&server_address, (socklen_t *)&addrlen)) < 0) {
-                perror("accept");
+        // If something happened on the server socket, it's an incoming connection
+        if (FD_ISSET(server_fd, &readfds)) {
+            if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+                perror("Accept failed");
                 exit(EXIT_FAILURE);
             }
 
-            printf("New connection, socket fd is %d, ip is : %s, port : %d\n",
-                   new_socket, inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
+            printf("New connection, socket fd is %d\n", new_socket);
 
-            // Add new socket to client_sockets array
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] == 0) {
-                    client_sockets[i] = new_socket;
-                    printf("Adding to list of sockets as %d\n", i);
-                    break;
-                }
-            }
-        }
+            // Find top two CPU-consuming processes
+            ProcessInfo procs[MAX_CLIENTS];
+            find_top_processes(procs, MAX_CLIENTS);
 
-        // Check if any of the client sockets have data to read
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            int sd = client_sockets[i];
+            // Sort the processes by CPU usage (highest first)
+            qsort(procs, MAX_CLIENTS, sizeof(ProcessInfo), compare_processes);
 
-            if (FD_ISSET(sd, &readfds)) {
-                char buffer[1024] = {0};
-                int valread = read(sd, buffer, 1024);
+            // Create a response with the top two processes
+            char response[BUFFER_SIZE];
+            snprintf(response, sizeof(response),
+                     "Top 2 CPU-consuming processes:\n1. PID: %d, Name: %s, CPU Time: %ld\n2. PID: %d, Name: %s, CPU Time: %ld\n",
+                     procs[0].pid, procs[0].name, procs[0].user_time + procs[0].kernel_time,
+                     procs[1].pid, procs[1].name, procs[1].user_time + procs[1].kernel_time);
 
-                if (valread == 0) {
-                    // Connection closed
-                    printf("Client disconnected, socket fd is %d\n", sd);
-                    close(sd);
-                    client_sockets[i] = 0;
-                } else {
-                    buffer[valread] = '\0';
-                    printf("Message received: %s\n", buffer);
+            // Send the response to the client
+            send(new_socket, response, strlen(response), 0);
+            printf("Sent top CPU-consuming processes to the client\n");
 
-                    // Find top two CPU-consuming processes
-                    ProcessInfo procs[2];
-                    find_top_processes(procs, 2);
-                    char message[1024];
-                    sprintf(message, "Top two CPU-consuming processes:\n"
-                                     "1. Name: %s, PID: %d, User Time: %ld, Kernel Time: %ld\n"
-                                     "2. Name: %s, PID: %d, User Time: %ld, Kernel Time: %ld\n",
-                                     procs[0].name, procs[0].pid, procs[0].user_time, procs[0].kernel_time,
-                                     procs[1].name, procs[1].pid, procs[1].user_time, procs[1].kernel_time);
-
-                    // Send the message back to the client
-                    send(sd, message, strlen(message), 0);
-                    printf("Message sent: %s\n", message);
-                }
-            }
+            // Close the socket after sending the information
+            close(new_socket);
         }
     }
 
-    close(server_socket);
     return 0;
 }
